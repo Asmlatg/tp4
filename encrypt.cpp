1f3a0029fce3833bca49a088436d7895c3ef2348
Asmlatg <116982968+Asmlatg@users.noreply.github.com>
2022-12-23 02:24:38 +0100

    Update encrypt.cpp

--- encrypt.cpp
+++ encrypt.cpp
@@ -1,8 +1,33 @@
 #include "encrypt.h"
 #include <string>
+#include <fstream>
+using namespace std;
+std::string read(std::string directory)
+{
+    ifstream flux;
+    flux.open(directory);
+    string ligne;
+    string filedata=" ";
+    while(getline(flux, ligne))
+      {
+         filedata += ligne;
+      }
+    return filedata;
+}
+
+
 encrypt::encrypt(std::string plain)
 {
-    _plain=plain;
+    ifstream monFlux;
+    monFlux.open(plain);
+    if(monFlux)
+    {
+        _plain=read(plain);
+    }
+    else
+    {
+        _plain=plain;
+    }
 }
 void encrypt::uncoded()
 {
